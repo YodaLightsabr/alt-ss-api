6014f820ccf86fde1e39f3fad34ed30b658f4542
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-07-18 03:03:58 -0700

    font

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -18,6 +18,12 @@ export async function getScreenshot(url, width, height) {
     const page = await getPage();
     await page.goto(url);
     await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720 });
+    await page.addStyleTag({ content: `
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+body, div {
+    font-family: 'Poppins', sans-serif;
+}
+    ` });
     const file = await page.screenshot({ fullPage: true });
     return file;
 }
